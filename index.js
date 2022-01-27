684a2e0fcadf3f55a0e28f7255b74a11c669d477
Caroline <anacdolima@gmail.com>
2022-01-27 15:08:33 -0300

    feat: get /participants

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-import express from "express"
+import express, { json } from "express"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config()
-const app = express()
+const server = express()
+server.use(json())
 
-app.get("/", (req, res) => {
-  res.send("Hello world!")
+server.get("/participants", async (req, res) => {
+  let mongoClient 
+  try{
+    mongoClient = new MongoClient(process.env.MONGO_URI)
+    await mongoClient.connect()
+    const db = mongoClient.db("BatePapo")
+    const parcipantsCollection = db.collection("participants")
+    const participants = await parcipantsCollection.find({}).toArray()
+    
+    res.send(participants)
+    mongoClient.close()
+  } catch {
+    console.log("Falha  na conexão com o servidor")
+    res.sendStatus(500)
+    mongoClient.close()
+  }
 })
 
-app.listen(5000)
+server.listen(5000, () => console.log("Listening on port 5000"))
